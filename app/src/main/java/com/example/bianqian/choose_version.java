3dcb17ff73519cf02c891ba1227ef267068c9c4c
lxy0806 <46130421+lxy0806@users.noreply.github.com>
2018-12-27 20:33:51 +0800

    first commit

---
+++ app/src/main/java/com/example/bianqian/choose_version.java
@@ -0,0 +1,10 @@
+package com.example.bianqian;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+public class choose_version extends AppCompatActivity {
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.choose);
+        }
+}
